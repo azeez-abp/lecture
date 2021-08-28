19b4dc3cdbf80ae54ae47397fecc8b129fa900a4
azeez-abp <adioadeyoriazeez@gmail.com>
2021-08-28 17:53:52 +0100

    New modification

--- index.php
+++ index.php
@@ -7,6 +7,25 @@ class Index{
     }
 
 }  
+?>
 
 
-Index::echo("WELCOME PHP");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="/" method="post">
+  <input type="email" name="e"  placeholder="Enter Email">
+  <input type="password" name="p"  placeholder="Enter Password">
+
+  <input type="submit" name="s" >
+</form>
+    
+</body>
+</html>
